1b5377c748eb89786ca965f8ed5b27b5686f46df
Michael Iduku <miduku@swwipe.com>
2025-06-13 16:03:12 +0100

    new update

--- src/components/features.jsx
+++ src/components/features.jsx
@@ -30,31 +30,59 @@ const DisplayText = ({ className, text, maxLength }) => {
 export const Features = (props) => {
   return (
     <div id="features" className="text-center padding-top-70">
-      <div className="container">
+      <div className="">
         <div className="col-md-10 col-md-offset-1 section-title">
           <h2>Best Sellers</h2>
         </div>
+        {/* <div className="row">
+          <div className="col-md-6 bg-div">
+            <h3>Vitamin C</h3>
+            <p>
+              Discover our top-selling products that have captured the hearts of our customers. Each item is crafted with care and designed to meet your needs.
+            </p>
+          </div>
+          <div className="col-md-6 bg-div">
+            <h3>Vitamin D</h3>
+            <p>
+              Discover our top-selling products that have captured the hearts of our customers. Each item is crafted with care and designed to meet your needs.
+            </p>
+          </div>
+        </div> */}
         <div style={{ marginBottom: "70px" }} className="row">
           {props.data
             ? props.data.map((d, i) => (
-                <div style={{ marginBottom: "25px" }} key={`${d.title}-${i}`} className="col-xs-6 col-md-3">
-                  {" "}
-                  <div className="card flex justify-content-center">
-                    <Card
-                      title={d.title}
-                      // subTitle="Card subtitle"
-                      footer={
-                        <a href={d.url} className="btn btn-custom btn-lg page-scroll">
-                          Buy now
-                        </a>
-                      }
-                      header={<img alt="Card" src={props.data ? process.env.PUBLIC_URL + "/" + d.image : ""} />}
-                      className="md:w-25rem"
-                    >
-                      <DisplayText className="m-0" text={d.text} maxLength={200} />
-                    </Card>
+                <div
+                  className="col-md-6 bg-div"
+                  style={{ background: `url(${process.env.PUBLIC_URL + "/" + d.image}) center center no-repeat`, backgroundSize: "cover" }}
+                  key={`${d.title}-${i}`}
+                >
+                  <div class="overlay-div" style={{ backgroundColor: d.bgOverlay }}></div>
+                  <div className="text-div">
+                    <h3>{d.title}</h3>
+                    {/* <DisplayText className="m-0" text={d.text} maxLength={200} /> */}
+                    <a href={d.url} className="btn btn-custom btn-lg page-scroll">
+                      Buy now
+                    </a>
                   </div>
                 </div>
+                // <div style={{ marginBottom: "25px" }} key={`${d.title}-${i}`} className="col-xs-6 col-md-3">
+                //   {" "}
+                //   <div className="card flex justify-content-center">
+                //     <Card
+                //       title={d.title}
+                //       // subTitle="Card subtitle"
+                //       footer={
+                //         <a href={d.url} className="btn btn-custom btn-lg page-scroll">
+                //           Buy now
+                //         </a>
+                //       }
+                //       header={<img alt="Card" src={props.data ? process.env.PUBLIC_URL + "/" + d.image : ""} />}
+                //       className="md:w-25rem"
+                //     >
+                //       <DisplayText className="m-0" text={d.text} maxLength={200} />
+                //     </Card>
+                //   </div>
+                // </div>
               ))
             : "Loading..."}
         </div>
--- src/components/gallery.jsx
+++ src/components/gallery.jsx
@@ -28,7 +28,12 @@ export const Gallery = (props) => {
         <div className="row">
           <div className="portfolio-items">
             <div className="col-sm-3 col-md-3 col-lg-3">
-                <Image className="hg" title={"Bethema"} largeImage={process.env.PUBLIC_URL + "/" + "img/portfolio/13.jpg"} smallImage={process.env.PUBLIC_URL + "/" + "img/portfolio/13.jpg"} />
+              <Image
+                className="hg"
+                title={"Bethema"}
+                largeImage={process.env.PUBLIC_URL + "/" + "img/portfolio/13.jpg"}
+                smallImage={process.env.PUBLIC_URL + "/" + "img/portfolio/13.jpg"}
+              />
             </div>
             <div className="col-sm-3 col-md-3 col-lg-3 hg-text">
               {/* <Image title="dummy" largeImage={process.env.PUBLIC_URL + "/" + d.largeImage} smallImage={process.env.PUBLIC_URL + "/" + d.smallImage} /> */}
@@ -36,11 +41,18 @@ export const Gallery = (props) => {
                 <img src={process.env.PUBLIC_URL + "/" + "img/leaf.svg"} />
               </div>
               <h5>K-Made For Us, Made By Us</h5>
-              <p>We’re building a space where melanated women are celebrated and empowered to take control of their skincare journey. Our products are tested and approved by melanated women whose voices have historically been sidelined in beauty and health conversations worldwide.</p>
-
+              <p>
+                We’re building a space where melanated women are celebrated and empowered to take control of their skincare journey. Our products are tested and
+                approved by melanated women whose voices have historically been sidelined in beauty and health conversations worldwide.
+              </p>
             </div>
             <div className="col-sm-3 col-md-3 col-lg-3">
-                <Image className="hg" title={"Bethema"} largeImage={process.env.PUBLIC_URL + "/" + "img/portfolio/16.jpg"} smallImage={process.env.PUBLIC_URL + "/" + "img/portfolio/16.jpg"} />
+              <Image
+                className="hg"
+                title={"Bethema"}
+                largeImage={process.env.PUBLIC_URL + "/" + "img/portfolio/16.jpg"}
+                smallImage={process.env.PUBLIC_URL + "/" + "img/portfolio/16.jpg"}
+              />
             </div>
             <div className="col-sm-3 col-md-3 col-lg-3 hg-text">
               {/* <Image title="dummy" largeImage={process.env.PUBLIC_URL + "/" + d.largeImage} smallImage={process.env.PUBLIC_URL + "/" + d.smallImage} /> */}
@@ -48,26 +60,35 @@ export const Gallery = (props) => {
                 <img src={process.env.PUBLIC_URL + "/" + "img/ppe.svg"} />
               </div>
               <h5>Rooted in Africa, Powered by Science</h5>
-              <p>We merge world-class formulations and global best practices with the healing benefits of African botanicals to give your skin the nourishment it deserves and to deliver products clinically proven to work on melanated skin</p>
-
+              <p>
+                We merge world-class formulations and global best practices with the healing benefits of African botanicals to give your skin the nourishment it
+                deserves and to deliver products clinically proven to work on melanated skin
+              </p>
             </div>
           </div>
         </div>
-<br/>
-         <div className="row">
+        <br />
+        <div className="row">
           <div className="portfolio-items">
-<div className="col-sm-3 col-md-3 col-lg-3 hg-text">
+            <div className="col-sm-3 col-md-3 col-lg-3 hg-text">
               {/* <Image title="dummy" largeImage={process.env.PUBLIC_URL + "/" + d.largeImage} smallImage={process.env.PUBLIC_URL + "/" + d.smallImage} /> */}
               <div className="xs">
                 <img src={process.env.PUBLIC_URL + "/" + "img/com.svg"} />
               </div>
               <h5>Unapologetic Jo</h5>
-              <p>WHarnessing the power of affirmations, bold packaging, and compelling yet honest stories from our community, we redefine your daily routine into a joyous and inspiring ceremony.</p>
-
+              <p>
+                WHarnessing the power of affirmations, bold packaging, and compelling yet honest stories from our community, we redefine your daily routine into
+                a joyous and inspiring ceremony.
+              </p>
             </div>
 
             <div className="col-sm-3 col-md-3 col-lg-3">
-                <Image className="hg" title={"Jessica"} largeImage={process.env.PUBLIC_URL + "/" + "img/portfolio/15.jpg"} smallImage={process.env.PUBLIC_URL + "/" + "img/portfolio/15.jpg"} />
+              <Image
+                className="hg"
+                title={"Jessica"}
+                largeImage={process.env.PUBLIC_URL + "/" + "img/portfolio/15.jpg"}
+                smallImage={process.env.PUBLIC_URL + "/" + "img/portfolio/15.jpg"}
+              />
             </div>
             <div className="col-sm-3 col-md-3 col-lg-3 hg-text">
               {/* <Image title="dummy" largeImage={process.env.PUBLIC_URL + "/" + d.largeImage} smallImage={process.env.PUBLIC_URL + "/" + d.smallImage} /> */}
@@ -75,18 +96,29 @@ export const Gallery = (props) => {
                 <img src={process.env.PUBLIC_URL + "/" + "img/afro.svg"} />
               </div>
               <h5>Real is Revolutionary</h5>
-              <p>We refuse to airbrush our models or uphold antiquated ideals. We’re here to inspire confidence and encourage growth by loving and celebrating what’s real.</p>
-
+              <p>
+                We refuse to airbrush our models or uphold antiquated ideals. We’re here to inspire confidence and encourage growth by loving and celebrating
+                what’s real.
+              </p>
             </div>
             <div className="col-sm-3 col-md-3 col-lg-3">
-                <Image className="hg" title={"Vitamin C Serum"} largeImage={process.env.PUBLIC_URL + "/" + "img/portfolio/14.jpg"} smallImage={process.env.PUBLIC_URL + "/" + "img/portfolio/14.jpg"} />
+              <Image
+                className="hg"
+                title={"Vitamin C Serum"}
+                largeImage={process.env.PUBLIC_URL + "/" + "img/portfolio/14.jpg"}
+                smallImage={process.env.PUBLIC_URL + "/" + "img/portfolio/14.jpg"}
+              />
             </div>
           </div>
-          </div>
+        </div>
       </div>
-      <div className="row">
+      <div className="row sticky-bottom">
         <div className="stn-custom marquee">
-          <h6> I AM RESTORED / I AM RESTORED / I AM RESTORED / I AM RESTORED / I AM RESTORED / I AM RESTORED / I AM RESTORED / I AM RESTORED / I AM RESTORED / I AM RESTORED /  I AM RESTORED /  I AM RESTORED /  I AM RESTORED /  I AM RESTORED / </h6>
+          <h6>
+            {" "}
+            I AM RESTORED / I AM RESTORED / I AM RESTORED / I AM RESTORED / I AM RESTORED / I AM RESTORED / I AM RESTORED / I AM RESTORED / I AM RESTORED / I AM
+            RESTORED / I AM RESTORED / I AM RESTORED / I AM RESTORED / I AM RESTORED /{" "}
+          </h6>
         </div>
       </div>
     </div>
