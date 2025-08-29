c4fc9cb0771abf60f1906fe8d91e9c77f78f9922
Michael Iduku <miduku@swwipe.com>
2025-08-29 09:41:35 +0100

    Updated instagram postings

--- src/components/instagram.jsx
+++ src/components/instagram.jsx
@@ -4,30 +4,10 @@ import { InstagramEmbed } from "react-social-media-embed";
 const InstagramCarousel = () => {
   return (
     <div className="instagram-container padding-bottom-70">
-      <InstagramEmbed
-        className="instagram-embed"
-        url="https://www.instagram.com/reel/C6UEe-rM0gc/?utm_source=ig_embed&amp;utm_campaign=loading"
-        width={328}
-        height={608}
-      />
-      <InstagramEmbed
-        className="instagram-embed"
-        url="https://www.instagram.com/reel/C5WQRb_oMTH/?utm_source=ig_embed&amp;utm_campaign=loading"
-        width={328}
-        height={608}
-      />
-      <InstagramEmbed
-        className="instagram-embed"
-        url="https://www.instagram.com/reel/C4upcQZINI5/?utm_source=ig_embed&amp;utm_campaign=loading"
-        width={328}
-        height={608}
-      />
-      <InstagramEmbed
-        className="instagram-embed"
-        url="https://www.instagram.com/reel/C5ZLNQmML2a/?utm_source=ig_embed&amp;utm_campaign=loading"
-        width={328}
-        height={608}
-      />
+      <InstagramEmbed className="instagram-embed" url="https://www.instagram.com/reel/DN6CAaQjdIx/?igsh=MTB0NG95dGV3bjhwbQ==" width={328} height={608} />
+      <InstagramEmbed className="instagram-embed" url="https://www.instagram.com/p/DL-W3VWouIh/?igsh=MXhvemFzMXV1ang5aQ==" width={328} height={608} />
+      <InstagramEmbed className="instagram-embed" url="https://www.instagram.com/reel/DCE31-fO72k/?igsh=anZxanUxdnpuOGJk" width={328} height={608} />
+      <InstagramEmbed className="instagram-embed" url="https://www.instagram.com/reel/DJ4KGjLI5Z-/?igsh=cnlodWo3MHg5cndi" width={328} height={608} />
     </div>
   );
 };
